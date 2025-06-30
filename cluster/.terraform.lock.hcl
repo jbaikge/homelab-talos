afcdef2c09724d5ecd313e6f8050caf177673753
jake <523216+jbaikge@users.noreply.github.com>
2025-06-30 10:49:35 -0400

    feat: add initial talos linux terraform

---
+++ cluster/.terraform.lock.hcl
@@ -0,0 +1,25 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/siderolabs/talos" {
+  version     = "0.8.1"
+  constraints = "0.8.1"
+  hashes = [
+    "h1:rhNnAJ4MLaKZ5hNbdFzUJYxEFg0Es5+jg+WlzQ1D/g4=",
+    "zh:02aeea4001ea216d37fd948e0760971f2525d31609d75dd1a7871f483e43260d",
+    "zh:0bd6d2f9b6daf9cec0e20d1e22cad635983b5c071c106a3bec51be283c9fa254",
+    "zh:0fa82a384b25a58b65523e0ea4768fa1212b1f5cfc0c9379d31162454fedcc9d",
+    "zh:290ced18cfa372681d53522b5ea6c392206e90181ef0884719768b3ef627d077",
+    "zh:3270a27a483d2be332915e339b910a4810fb16505e060fc4a988b0d653f06d90",
+    "zh:34f91c967ae25219abc81d21a477d3fc514c62a73084bfb9d3d2d1490e98070b",
+    "zh:46a0eb4397e97d9dc354087ce1b16ccdca3876b0e339d7ec1919002a43aa0a6d",
+    "zh:4d382770c97675c6b4355a91f50b38f9b6bd088707834c9efb308e608bbdae48",
+    "zh:6ec2828c419615cce850ba1eafffee2797cae62876999f3b0a163c17f579c97a",
+    "zh:9405b011c631d9fb001d8b96a1657e071181434960543e857fb14ec2230618b0",
+    "zh:a9a6f6824793e811ec52c0b1c4b8d19855c851120a91f7617ffa7e36aa65710a",
+    "zh:aa472818c7880c7cf19f5bd584dcf513a3daa2fe636d4af730b0bcf495eadebf",
+    "zh:cd6037f6267987fb606f98b1a425d71a1826289ac39e62973a45b60f0f37de06",
+    "zh:ddea6372fef17de6648018c4c64b87acecaba9f5443fcf46ff3d92c048605b30",
+    "zh:e367b0359c8b413f705ded1d0d7b4a3c09cee1bd0028337faa80a150b08b945a",
+  ]
+}
---
+++ cluster/main.tf
@@ -0,0 +1,80 @@
+locals {
+  cluster_endpoint = "https://${var.cluster_name}.${var.root_domain}:6443"
+}
+
+resource "talos_machine_secrets" "this" {}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = local.cluster_endpoint
+  machine_type     = "controlplane"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_machine_configuration" "worker" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = local.cluster_endpoint
+  machine_type     = "worker"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+data "talos_client_configuration" "this" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.this.client_configuration
+  endpoints            = [for k, v in var.nodes.controlplanes : "${k}.${var.root_domain}"]
+}
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  for_each                    = var.nodes.controlplanes
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.controlplane.machine_configuration
+  node                        = each.key
+  endpoint                    = "${each.key}.${var.root_domain}"
+  config_patches = [
+    templatefile("${path.module}/templates/disk.yaml", {
+      install_disk = each.value.install_disk
+    }),
+    templatefile("${path.module}/templates/network.yaml", {
+      dns_domain = var.root_domain
+      ip_address = each.value.ip_address
+      hostname   = each.key
+    }),
+    templatefile("${path.module}/templates/cluster-subnet.yaml", {}),
+    templatefile("${path.module}/templates/control-plane-scheduling.yaml", {}),
+  ]
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  for_each                    = var.nodes.workers
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  node                        = each.key
+  endpoint                    = "${each.key}.${var.root_domain}"
+  config_patches = [
+    templatefile("${path.module}/templates/disk.yaml", {
+      install_disk = each.value.install_disk
+    }),
+    templatefile("${path.module}/templates/network.yaml", {
+      dns_domain = var.root_domain
+      ip_address = each.value.ip_address
+      hostname   = each.key
+    }),
+    templatefile("${path.module}/templates/cluster-subnet.yaml", {}),
+  ]
+}
+
+resource "talos_machine_bootstrap" "this" {
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k, v in var.nodes.controlplanes : "${k}.${var.root_domain}"][0]
+  depends_on = [
+    talos_machine_configuration_apply.controlplane,
+  ]
+}
+
+resource "talos_cluster_kubeconfig" "this" {
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = [for k, v in var.nodes.controlplanes : "${k}.${var.root_domain}"][0]
+  depends_on = [
+    talos_machine_bootstrap.this,
+  ]
+}
