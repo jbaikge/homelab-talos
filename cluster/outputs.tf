c0d40b28c03cbfea87320865d42db6bea3f918cf
jake <523216+jbaikge@users.noreply.github.com>
2025-06-30 15:14:51 -0400

    fix: remove machineconfig from outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -7,7 +7,3 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
-
-output "machineconfig" {
-  value = data.talos_machine_configuration.this
-}
