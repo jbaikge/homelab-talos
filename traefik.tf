9b99ff1b66fd8d78d7c1678722901e0c7a36ebf8
jake <523216+jbaikge@users.noreply.github.com>
2025-07-03 10:03:34 -0400

    feat: add traefik

---
+++ traefik.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_namespace" "traefik" {
+  metadata {
+    name = "traefik"
+  }
+
+  depends_on = [
+    data.talos_cluster_health.this,
+  ]
+}
+
+resource "helm_release" "traefik" {
+  name       = "traefik"
+  repository = "https://traefik.github.io/charts"
+  chart      = "traefik"
+  version    = "36.2.0"
+  namespace  = kubernetes_namespace.traefik.metadata[0].name
+
+  values = [
+    templatefile("${path.module}/templates/traefik.yml", {}),
+  ]
+
+  depends_on = [
+    kubectl_manifest.L2Advertisement,
+  ]
+}
