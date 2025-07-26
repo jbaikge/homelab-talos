90cc1738bde6b98f441a50cce1920092971212b6
jake <523216+jbaikge@users.noreply.github.com>
2025-07-26 07:36:54 -0400

    refactor: change metallb namespace to ingress

--- apps/namespaces.tf
+++ apps/namespaces.tf
@@ -16,12 +16,15 @@ resource "kubernetes_namespace" "database" {
   }
 }
 
-resource "kubernetes_namespace" "ingress" {
+resource "kubernetes_namespace" "metallb-system" {
+  # depends_on = [data.talos_cluster_health.health]
   metadata {
-    name = "ingress"
-    annotations = {
-      name = "ingress"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
     }
+    name = "metallb-system"
   }
 }
 
