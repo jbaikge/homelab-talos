091c9aa6b15d3f39ed2e5bfe9cf3d3adab412af7
jake <523216+jbaikge@users.noreply.github.com>
2025-07-26 23:26:03 -0400

    fix: update path substitutions

--- sops/decrypt.sh
+++ sops/decrypt.sh
@@ -5,5 +5,5 @@ set -e
 DIR=$(dirname "$0")
 
 for FILE in $DIR/*.enc.yml; do
-    sops decrypt --output "${FILE/.enc.yml$/.dec.yml}" "$FILE"
+    sops decrypt --output "${FILE/.enc.yml/.dec.yml}" "$FILE"
 done
--- sops/encrypt.sh
+++ sops/encrypt.sh
@@ -5,5 +5,5 @@ set -e
 DIR=$(dirname "$0")
 
 for FILE in $DIR/*.dec.yml; do
-    sops encrypt --output "${FILE/.dec.yml$/.enc.yml}" "$FILE"
+    sops encrypt --output "${FILE/.dec.yml/.enc.yml}" "$FILE"
 done
