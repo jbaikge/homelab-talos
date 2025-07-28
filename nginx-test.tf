191475664dfaa113afb39661ddc9acfaf5264db1
jake <523216+jbaikge@users.noreply.github.com>
2025-07-28 10:05:01 -0400

    feat: finish defining nginx-test deployment

--- nginx-test.tf
+++ nginx-test.tf
@@ -4,22 +4,22 @@ resource "kubernetes_namespace" "nginx-test" {
   }
 }
 
-resource "kubernetes_deployment" "nginx" {
+resource "kubernetes_deployment" "nginx-test" {
   metadata {
-    name      = "nginx"
+    name      = "nginx-test"
     namespace = kubernetes_namespace.nginx-test.metadata[0].name
   }
   spec {
     replicas = 1
     selector {
       match_labels = {
-        app = "nginx"
+        app = "nginx-test"
       }
     }
     template {
       metadata {
         labels = {
-          app = "nginx"
+          app = "nginx-test"
         }
       }
       spec {
@@ -31,6 +31,65 @@ resource "kubernetes_deployment" "nginx" {
           }
         }
       }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx-test" {
+  metadata {
+    name      = "nginx-test"
+    namespace = kubernetes_namespace.nginx-test.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = "nginx-test"
+    }
+    port {
+      port = 80
+    }
+  }
+
+  depends_on = [
+    kubernetes_deployment.nginx-test,
+  ]
+}
+
+resource "kubernetes_manifest" "nginx-test_httproute" {
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind       = "HTTPRoute"
+    metadata = {
+      name      = "nginx-test"
+      namespace = kubernetes_namespace.nginx-test.metadata[0].name
+    }
+    spec = {
+      parentRefs = [
+        {
+          name      = "traefik-gateway"
+          namespace = kubernetes_namespace.traefik.metadata[0].name
+        },
+      ]
+      hostnames = [
+        "nginx-test.${var.domain}",
+      ]
+      rules = [
+        {
+          matches = [
+            {
+              path = {
+                type  = "PathPrefix"
+                value = "/"
+              }
+            },
+          ]
+          backendRefs = [
+            {
+              name = "nginx-test"
+              port = 80
+            },
+          ]
+        },
+      ]
     }
   }
 }
