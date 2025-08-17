f3ea6392bf3854a4ced746f5ee1e9877fde1b3b0
jake <523216+jbaikge@users.noreply.github.com>
2025-08-17 17:30:51 -0400

    feat: add cloudflare variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -108,3 +108,25 @@ variable "cloudflared_tunnel_token" {
   type        = string
   sensitive   = true
 }
+
+variable "cloudflare_zone" {
+  description = "Domain used to expose services to the Internet"
+  type        = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Zone ID for your domain"
+  type        = string
+}
+
+variable "cloudflare_account_id" {
+  description = "Account ID for your Cloudflare account"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_email" {
+  description = "Email address for your Cloudflare account"
+  type        = string
+  sensitive   = true
+}
