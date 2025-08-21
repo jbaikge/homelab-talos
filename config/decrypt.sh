0d5b9987323a9b577ce0cca960e199d1f7eb055b
jake <523216+jbaikge@users.noreply.github.com>
2025-08-20 20:10:10 -0400

    refactor: remove en/decrypt tools

--- config/decrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DIR=$(dirname "$0")
-
-for FILE in $DIR/*.enc.yml; do
-    sops decrypt --output "${FILE/.enc.yml/.dec.yml}" "$FILE"
-done
--- config/encrypt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DIR=$(dirname "$0")
-
-for FILE in $DIR/*.dec.yml; do
-    sops encrypt --output "${FILE/.dec.yml/.enc.yml}" "$FILE"
-done
