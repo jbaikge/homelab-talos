632dcf3e012b5b8f3f15466d7ea9a9339b406dc1
jake <523216+jbaikge@users.noreply.github.com>
2025-09-08 17:09:02 -0400

    feat: small script to download calendar card pro

--- apps/home-assistant/calendar-card.sh
+++ apps/home-assistant/calendar-card.sh
@@ -5,5 +5,6 @@ set -ex
 VERSION="v3.1.0"
 URL="https://github.com/alexpfau/calendar-card-pro/releases/download/${VERSION}/calendar-card-pro.js"
 FILENAME=$(basename "$URL")
+DIR=/config/www
 
-curl --silent --location --output "$FILENAME" "$URL"
+kubectl --namespace home-assistant exec home-assistant-6954bb44b-9l4q6 -- sh -c "mkdir -p $DIR && wget -O $DIR/$FILENAME $URL"
